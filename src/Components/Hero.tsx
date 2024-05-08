f65f1279f5b025c3694d00e769afc005f6239512
Gaurav Kumar <gaurav2k9ssk4754@gmail.com>
2024-05-08 17:55:39 +0530

    added navigation for mix and master

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,12 +2,13 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import { staticAssets } from "@/Utills/staticAssets";
 import Info from "./Info";
+import Link from 'next/link';
 
 export default function Hero() {
   return (
     <Box bgColor="white" pt="10vh">
-      <Box w="100%" h="30vh" position="relative" mb="5vh" cursor="pointer" onClick={()=>{
-            window.open("https://calendly.com/","_blank")
+      <Box w="100%" h="30vh" position="relative" mb="5vh" cursor="pointer" onClick={() => {
+        window.open("https://calendly.com/", "_blank")
       }} >
         <Image
           src={staticAssets.hero1}
@@ -19,14 +20,14 @@ export default function Hero() {
           __css={{
             filter: " blur(0.5px) contrast(80%) ",
           }}
-          
+
         />
         <Text
           position="absolute"
           top="50%"
           left="50%"
-        //   fontSize="5xl"
-          fontSize={{ base: "2xl", md: "3xl" , lg:"5xl" }}
+          //   fontSize="5xl"
+          fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
           fontWeight="600"
           color="white"
           transform="translate(-50%, -50%)"
@@ -34,30 +35,32 @@ export default function Hero() {
           Book a Session
         </Text>
       </Box>
-      <Box w="100%" h="30vh" position="relative" mb="5vh" cursor="pointer" >
-        <Image
-          src={staticAssets.hero2}
-          alt="hero-1"
-          w="100%"
-          h="100%"
-          objectFit="cover"
-          zIndex="-1"
-          __css={{
-            filter: " blur(0.5px) contrast(80%) ",
-          }}
-        />
-        <Text
-          position="absolute"
-          top="50%"
-          left="50%"
-          fontSize={{ base: "2xl", md: "3xl" , lg:"5xl" }}
-          fontWeight="600"
-          color="white"
-          transform="translate(-50%, -50%)"
-        >
-          Mix and Master 
-        </Text>
-      </Box>
+      <Link href="/mixmaster">
+        <Box w="100%" h="30vh" position="relative" mb="5vh" cursor="pointer">
+          <Image
+            src={staticAssets.hero2}
+            alt="hero-1"
+            w="100%"
+            h="100%"
+            objectFit="cover"
+            zIndex="-1"
+            __css={{
+              filter: " blur(0.5px) contrast(80%) ",
+            }}
+          />
+          <Text
+            position="absolute"
+            top="50%"
+            left="50%"
+            fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
+            fontWeight="600"
+            color="white"
+            transform="translate(-50%, -50%)"
+          >
+            Mix and Master
+          </Text>
+        </Box>
+      </Link>
       <Box w="100%" h="30vh" position="relative" cursor="pointer" >
         <Image
           src={staticAssets.hero3}
@@ -69,13 +72,13 @@ export default function Hero() {
           __css={{
             filter: " blur(0.5px) contrast(80%) ",
           }}
-          
+
         />
         <Text
           position="absolute"
           top="50%"
           left="50%"
-          fontSize={{ base: "2xl", md: "3xl" , lg:"5xl" }}
+          fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
           fontWeight="600"
           color="white"
           transform="translate(-50%, -50%)"
@@ -83,7 +86,7 @@ export default function Hero() {
           Courses
         </Text>
       </Box>
-      <Info/>
+      <Info />
     </Box>
   );
 }
